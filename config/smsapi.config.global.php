6b1578e01653d63e2b6c748b07db1088c397cf2f
pontifex <lucas.vavrinak@gmail.com>
2013-05-06 09:12:38 +0200

    Copied old SVN repository.

---
+++ config/smsapi.config.global.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author lucas.wawrzyniak
+ * @copyright Copyright (c) 2013 Lucas Wawrzyniak
+ * @licence New BSD License
+ */
+return array(
+    'smsapi' => array(
+        'url' => 'http://api.smsapi.pl/send.do',
+        'username' => '',
+        'password' => '',
+    ),
+);
